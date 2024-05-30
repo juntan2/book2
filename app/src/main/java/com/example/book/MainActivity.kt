8ec819cb7beff149a26655dd6b91753c371d94a2
juntan <junyoung6515@naver.com>
2024-05-30 15:50:51 +0900

    init

---
+++ app/src/main/java/com/example/book/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.book
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val imageButton: ImageButton = findViewById(R.id.btn_T)
+        imageButton.setOnClickListener{
+            val intent = Intent(this,subT::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
